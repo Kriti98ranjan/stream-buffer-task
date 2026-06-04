e4aa5eb3be98eebd5801ad48cbe66ba289b306e5
Kriti <kritiranjan196@gmail.com>
2026-06-04 12:39:59 +0530

    feat: add introspect.js to display complete system information

---
+++ introspect.js
@@ -0,0 +1,10 @@
+const os = require("os"); 
+const nodePath = process.execPath; 
+console.log("OS:                  " + os.type() + " " + os.release()); 
+console.log("Architecture:        " + os.arch()); 
+console.log("CPU Cores:           " + os.cpus().length); 
+console.log("Total Memory:        " + (os.totalmem()/1024**3).toFixed(2) + " GB"); 
+console.log("System Uptime:       " + (os.uptime()/3600).toFixed(2) + " hours"); 
+console.log("Current Logged User: " + os.userInfo().username); 
+console.log("Node Path:           " + nodePath); 
+console.log("Logout Time:         " + new Date().toLocaleString()); 
